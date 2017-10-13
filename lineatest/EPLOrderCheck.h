0ce26a5d49d06bca7d61c110173f5296ab301dc5
DanielRod17 <32774292+DanielRod17@users.noreply.github.com>
2017-10-13 13:01:15 -0600

    Add files via upload

---
+++ lineatest/EPLOrderCheck.h
@@ -0,0 +1,18 @@
+//
+//  EPLOrderCheck.h
+//  ButtonTest
+//
+//  Created by itsupport on 10/12/17.
+//  Copyright (c) 2017 EP Logistics, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface EPLOrderCheck : UIViewController
+    
+
+- (IBAction)thAttempt:(id)sender;
+@property (retain, nonatomic) IBOutlet UITextField *Tokenito;
+
+@end
